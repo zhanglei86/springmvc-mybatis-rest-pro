c6d07a5a30d44f500fb4058f5abdce872bd28722
zhanglei86 <s_sir@qq.com>
2016-03-07 10:45:29 +0800

    优化

--- src/main/java/com/zl/mapper/datasource1/TCountryMapper.java
+++ src/main/java/com/zl/mapper/datasource1/TCountryMapper.java
@@ -1,7 +1,6 @@
 package com.zl.mapper.datasource1;
 
 import com.zl.model.datasource1.TCountry;
-
 import tk.mybatis.mapper.common.Mapper;
 
 public interface TCountryMapper extends Mapper<TCountry> {
--- src/main/java/com/zl/mapper/datasource2/TCountryMapper2.java
+++ src/main/java/com/zl/mapper/datasource2/TCountryMapper2.java
@@ -1,6 +1,5 @@
 package com.zl.mapper.datasource2;
 
-
 import com.zl.model.datasource2.TCountry;
 
 import tk.mybatis.mapper.common.Mapper;
